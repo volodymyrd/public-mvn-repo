378f2ca172d50dc43de028506218175e3aca6707
volodymyrd <volodymyrdotsenko@gmail.com>
2020-07-13 23:20:34 +0200

    add gradle wrapper.

---
+++ proto-utils/src/test/java/com/volmyr/proto/utils/ProtoUtilsTest.java
@@ -0,0 +1,36 @@
+package com.volmyr.proto.utils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.volmyr.proto.model.test.Person;
+import java.io.IOException;
+import org.junit.Test;
+
+/**
+ * Tests for {@link ProtoUtils}.
+ */
+public class ProtoUtilsTest {
+
+  @Test
+  public void shouldConvertProtoMessageToJsonString() throws IOException {
+    assertThat(
+        ProtoUtils.toJson(
+            Person.newBuilder()
+                .setFirstName("Name 1")
+                .setLastName("Name 2")
+                .build()))
+        .isEqualTo("{\"firstName\":\"Name 1\",\"lastName\":\"Name 2\"}");
+  }
+
+  @Test
+  public void shouldConvertJsonStringToProtoPerson() throws IOException {
+    assertThat(
+        ProtoUtils
+            .fromJson("{\"firstName\":\"Name 1\",\"lastName\":\"Name 2\"}", Person.newBuilder()))
+        .isEqualTo(
+            Person.newBuilder()
+                .setFirstName("Name 1")
+                .setLastName("Name 2")
+                .build());
+  }
+}
